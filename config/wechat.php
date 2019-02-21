2238e1f20534b73d722e978d90aa54f0b005aeb6
hzjdhr <782389483@qq.com>
2019-02-21 11:07:59 +0800

    detail

--- config/wechat.php
+++ config/wechat.php
@@ -16,6 +16,7 @@ return [
     /*
      * 账号基本信息，请从微信公众平台/开放平台获取
      */
+
     'app_id'  => env('WECHAT_APPID', 'your-app-id'),         // AppID
     'secret'  => env('WECHAT_SECRET', 'your-app-secret'),     // AppSecret
     'token'   => env('WECHAT_TOKEN', 'your-token'),          // Token
