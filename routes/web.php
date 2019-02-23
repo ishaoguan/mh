56d78a53b4d0b65a198aa5d559a9c3dccf61d3f5
hzjdhr <782389483@qq.com>
2019-02-23 10:01:14 +0800

    detail

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/listScript','IndexController@listScript');
+Route::get('/listScript/{id}/{num}','IndexController@listScript');
 
 Route::get('/','IndexController@index');
 Route::get('detail/{id}','IndexController@detail');
